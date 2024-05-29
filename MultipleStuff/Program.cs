ced0e8e038d070f3f5671b96f07e251369b1e90e
Daniela Sousa <danielasousa.info@gmail.com>
2024-05-29 12:00:40 +0100

    Criada instância da classe SuperList e adicionado os números

--- MultipleStuff/Program.cs
+++ MultipleStuff/Program.cs
@@ -5,8 +5,8 @@ namespace MultipleStuff
     class Program
     {
         static void Main(string[] args)
-        {
-
+        {        
+            SuperList superList = new SuperList { 3.5, 7.2, 1.8, 9.0, 2.4 };
         }
     }
 }
