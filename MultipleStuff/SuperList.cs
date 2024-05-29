a10b0a8e06942ff13d83132527027f9741bd8290
Daniela Sousa <danielasousa.info@gmail.com>
2024-05-29 11:51:25 +0100

    Método GetMinMax1 que usa parâmetros out

--- MultipleStuff/SuperList.cs
+++ MultipleStuff/SuperList.cs
@@ -3,8 +3,23 @@ using System.Collections.Generic;
 
 namespace MultipleStuff
 {
-    public class SuperList
+    public class SuperList : List<double>
     {
-        
+       public void GetMinMax1(out double min, out double max)
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("A lista está vazia.");
+            }
+
+            min = double.MaxValue;
+            max = double.MinValue;
+
+            foreach (var num in this)
+            {
+                if (num < min) min = num;
+                if (num > max) max = num;
+            } 
+        }
     }
 }
